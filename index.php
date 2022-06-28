e57fa0e6a0e06728e158889d5d3be43e9e3398d7
ahmad-fauzie <ahmad.ajah109@gmail.com>
2022-06-28 11:39:18 +0700

    Membuat home page

---
+++ index.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+
+    <script src="https://kit.fontawesome.com/e3dfa6f57a.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="container-fluid" id="bcgr">
+        <div class="container" id="background">
+            <img src="img/foto.jpg" class="mx-auto d-block" alt="">
+            <h1>Ahmad Fauzi</h1>
+            <h3>A last year student majoring in IT at one of the state 
+                universities in Jakarta who likes coding and programming. experience in java, php, javascript. 
+                Previously had an internship at a software house company
+            </h3>
+            
+            <a href="https://www.linkedin.com/in/ahmadfauzie"><i class="fa-brands fa-linkedin"></i></a>
+            <a href="https://www.instagram.com/fauzi_crwz"><i class="fa-brands fa-instagram"></i></a>
+            <a href="https://github.com/ahmad-fauzie"><i class="fa-brands fa-github"></i></a>
+        </div>
+        
+        <div class="container" id="skills">
+            <h2>Skills</h2>
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="img/java.png" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h4 class="card-titile">Java</h4>
+                          <p class="card-text"><b> 2 year </b>of experience</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="img/laravel.svg" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h4 class="card-titile">Laravel</h4>
+                          <p class="card-text"><b> 6 months </b>of experience</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="img/javascript.png" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h4 class="card-titile">Javascript</h4>
+                          <p class="card-text"><b> 2 months </b>of experience</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div class="row">
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="img/mysql.png" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h4 class="card-titile">MySql</h4>
+                          <p class="card-text"><b> 2 year </b>of experience</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-md-4">
+                    <div class="card">
+                        <img src="img/php.png" class="card-img-top" alt="">
+                        <div class="card-body">
+                            <h4 class="card-titile">PHP</h4>
+                          <p class="card-text"><b> 1 year </b>of experience</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            
+        </div>
+    </div>
+</body>
+
+</html>
